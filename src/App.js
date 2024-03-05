265a9b14a5521653f122f90014d306da213373bd
I Joo Han <juhanlee2218@gmail.com>
2024-03-05 23:02:52 +0900

    #13 [Enhancement] 페이지 렌더시 스크롤 위치 상단으로 이동하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { useState, useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Topbar from "./components/Topbar/Topbar.jsx";
 import { Main } from "./pages/Main/Main.jsx";
 import { Signup } from "./components/Signup.jsx";
@@ -16,22 +16,27 @@ import './App.css';
 
 function App() {
   const [isScroll, setIscroll] = useState(false);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 120) {
-              setIscroll(true);
-            } else {
-              setIscroll(false);
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+        if (window.scrollY > 120) {
+          setIscroll(true);
+        } else {
+          setIscroll(false);
+        }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 페이지가 이동될 때 항상 스크롤을 맨 위로 옮깁니다.
+  }, [navigate]);
 
   return (
     <div className="Body">
