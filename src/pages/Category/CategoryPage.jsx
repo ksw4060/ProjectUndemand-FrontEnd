6b6736c76bbaef8ab768b7efeaf8a31a30c117e6
I Joo Han <juhanlee2218@gmail.com>
2024-03-13 17:23:30 +0900

    #12 [Feat] Filter component 제작 - (모든 카테고리 페이지에서 filter 활성화 가능 + URL 파라미터를 활용해 카테고리 페이지 통합)

---
+++ src/pages/Category/CategoryPage.jsx
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./CategoryPage.css";
+import { RiEqualizerLine } from "react-icons/ri";
+import { FaChevronDown } from "react-icons/fa6";
+// import axios from 'axios';
+
+function CategoryPage() {
+    const [isCategoryScroll, setIsCategoryScroll] = useState(false);
+    const [isFilterClicked, setIsFilterClicked] = useState(false);
+    const { category } = useParams();
+    const upperCaseCategory = category.toUpperCase();
+
+    // 상품 데이터 받아온 이후에 사용 할 코드
+    // const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const handleCategoryScroll = () => {
+            if (window.scrollY > 150) {
+                setIsCategoryScroll(true);
+            } else {
+                setIsCategoryScroll(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleCategoryScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleCategoryScroll);
+        };
+    }, []);
+
+    // 상품 데이터 받아온 이후에 사용 할 코드
+    // useEffect(() => {
+    //     axios.get('/products')
+    //         .then(response => {
+    //             setProducts(response.data);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching products:', error);
+    //         });
+    // }, []);
+
+    const hanldeFilterClick = () => {
+        setIsFilterClicked(prevState => !prevState);
+    };
+
+    return (
+        <div className="category-page">
+            <div className={`title-section ${isCategoryScroll ? 'scroll-category' : ''}`}>
+                <div className="wrapper">
+                    <h2>{ upperCaseCategory } Products</h2>
+                    <div className="filter-box">
+                        <div className="filter" onClick={() => hanldeFilterClick()}>
+                            <p>필터 표시</p>
+                            <RiEqualizerLine className='icons'/>
+                        </div>
+                        <div className="sort-by">
+                            <p>정렬 기준</p>
+                            <FaChevronDown className='icons'/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={`contents-section ${isCategoryScroll ? 'scroll-category' : ''}`}>
+                <div className={`filter-section ${isFilterClicked && 'filter-active'}`}>
+                    <ul className="category-options">
+                        <li className='filter-option'>상의</li>
+                        <li className='filter-option'>하의</li>
+                        <li className='filter-option'>아우터</li>
+                        <li className='filter-option'>신발</li>
+                        <li className='filter-option'>악세서리</li>
+                    </ul>
+                    <ul className="price-options">
+                        <li className='filter-option-title'><span>가격대</span> <span>(0)</span></li>
+                        <li className='filter-option'><input type="radio" /><span>0 ~ 50,000 원</span></li>
+                        <li className='filter-option'><input type="radio" /><span>50,000 ~ 100,000 원</span></li>
+                        <li className='filter-option'><input type="radio" /><span>100,000 ~ 150,000 원</span></li>
+                        <li className='filter-option'><input type="radio" /><span>150,000 ~ 200,000 원</span></li>
+                    </ul>
+                    <ul className="color-options">
+                        <li className='filter-option-title'><span>색상</span> <span>(0)</span></li>
+                        <li className='filter-option'><input type="radio" /><span>블랙</span></li>
+                        <li className='filter-option'><input type="radio" /><span>화이트</span></li>
+                        <li className='filter-option'><input type="radio" /><span>네이비</span></li>
+                        <li className='filter-option'><input type="radio" /><span>그레이</span></li>
+                    </ul>
+                </div>
+                <div className="products-section">
+                    <ul className="product-card-box">
+                        <li className={`product-card ${isFilterClicked && "filter-active-margin"}`}>
+                            <div className={`img-section img1 ${isFilterClicked && 'filter-active-img'}`}></div>
+                            <div className="product-info">
+                                <Link to="/">Product Name</Link>
+                                <Link to="/">Price</Link>
+                            </div>
+                        </li>
+                        <li className={`product-card ${isFilterClicked && "filter-active-margin"}`}>
+                            <div className={`img-section img2 ${isFilterClicked && 'filter-active-img'}`}></div>
+                            <div className="product-info">
+                                <Link to="/">Product Name</Link>
+                                <Link to="/">Price</Link>
+                            </div>
+                        </li>
+                        <li className={`product-card ${isFilterClicked && "filter-active-margin"}`}>
+                            <div className={`img-section img3 ${isFilterClicked && 'filter-active-img'}`}></div>
+                            <div className="product-info">
+                                <Link to="/">Product Name</Link>
+                                <Link to="/">Price</Link>
+                            </div>
+                        </li>
+                        <li className={`product-card ${isFilterClicked && "filter-active-margin"}`}>
+                            <div className={`img-section img4 ${isFilterClicked && 'filter-active-img'}`}></div>
+                            <div className="product-info">
+                                <Link to="/">Product Name</Link>
+                                <Link to="/">Price</Link>
+                            </div>
+                        </li>
+                        <li className={`product-card ${isFilterClicked && "filter-active-margin"}`}>
+                            <div className={`img-section img5 ${isFilterClicked && 'filter-active-img'}`}></div>
+                            <div className="product-info">
+                                <Link to="/">Product Name</Link>
+                                <Link to="/">Price</Link>
+                            </div>
+                        </li>
+                        <li className={`product-card ${isFilterClicked && "filter-active-margin"}`}>
+                            <div className={`img-section img6 ${isFilterClicked && 'filter-active-img'}`}></div>
+                            <div className="product-info">
+                                <Link to="/">Product Name</Link>
+                                <Link to="/">Price</Link>
+                            </div>
+                        </li>
+                        <li className={`product-card ${isFilterClicked && "filter-active-margin"}`}>
+                            <div className={`img-section img7 ${isFilterClicked && 'filter-active-img'}`}></div>
+                            <div className="product-info">
+                                <Link to="/">Product Name</Link>
+                                <Link to="/">Price</Link>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* 상품 데이터 받아온 이후에 사용 할 코드 */}
+                {/* <ul className="product-card-box">
+                    {products.map(product => (
+                        <li key={product.id} className="product-card">
+                            <div className={`img-section img${product.id}`}></div>
+                            <div className="product-info">
+                                <Link to="/">{product.name}</Link>
+                                <Link to="/">{product.price}</Link>
+                            </div>
+                        </li>
+                    ))}
+                </ul> */}
+            </div>
+        </div>
+    );
+}
+
+export {CategoryPage};
