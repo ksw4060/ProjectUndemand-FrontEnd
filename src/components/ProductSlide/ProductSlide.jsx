f57ec2e1533ca0a5e9528d01a6b2b4a99c417bb2
I Joo Han <juhanlee2218@gmail.com>
2024-03-28 20:52:43 +0900

    [Modify] 백엔드 연동 1차 - (메인 페이지 연동) #28

--- src/components/ProductSlide/ProductSlide.jsx
+++ src/components/ProductSlide/ProductSlide.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./ProductSlide.css";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
-function ProductSlide({ title, slideItems }) {
+function ProductSlide({ products, sectionTitle }) {
     const [marginLeft, setMarginLeft] = useState(0);
     const slideUlRef = useRef(null);
 
@@ -12,46 +12,64 @@ function ProductSlide({ title, slideItems }) {
             setMarginLeft(-10);
             setTimeout(() => {
                 setMarginLeft(0);
-                slideUlRef.current.appendChild(slideUlRef.current.firstChild);
+                if (slideUlRef.current) {
+                    const firstChild = slideUlRef.current.firstChild;
+                    if (firstChild) {
+                        slideUlRef.current.appendChild(firstChild.cloneNode(true));
+                        slideUlRef.current.removeChild(firstChild);
+                    }
+                }
             }, 0);
         }, 10000);
         return () => clearInterval(interval);
     }, []);
 
     const moveLeft = () => {
-        slideUlRef.current.insertBefore(slideUlRef.current.lastChild, slideUlRef.current.firstChild);
-        setMarginLeft(-10);
-        setTimeout(() => setMarginLeft(0), 0);
+        if (slideUlRef.current) {
+            const lastChild = slideUlRef.current.lastChild;
+            if (lastChild) {
+                slideUlRef.current.insertBefore(lastChild.cloneNode(true), slideUlRef.current.firstChild);
+                slideUlRef.current.removeChild(lastChild);
+                setMarginLeft(-10);
+                setTimeout(() => setMarginLeft(0), 0);
+            }
+        }
     };
 
     const moveRight = () => {
         setMarginLeft(-10);
         setTimeout(() => {
             setMarginLeft(0);
-            slideUlRef.current.appendChild(slideUlRef.current.firstChild);
+            if (slideUlRef.current) {
+                const firstChild = slideUlRef.current.firstChild;
+                if (firstChild) {
+                    slideUlRef.current.appendChild(firstChild.cloneNode(true));
+                    slideUlRef.current.removeChild(firstChild);
+                }
+            }
         }, 0);
     };
 
     return (
         <section className="product-slide">
             <div className="small-title">
-                <h1>{title}</h1>
+                <h1>{sectionTitle}</h1>
             </div>
             <div className="products-box">
                 <ul ref={slideUlRef} className="product-slide-ul" style={{ marginLeft: marginLeft + "px" }}>
-                    {slideItems.map((item, index) => (
-                        <li key={index} className="product-card">
-                            <div className={`product-img${index + 1} product-card-img`}></div>
+                    {products.map((product, index) => (
+                        <li key={product.productId} className="product-card">
+                            <img className={`product-img${index + 1} product-card-img`} src={product.product_img} alt={product.productName} />
                             <div className="product-small-info">
-                                <Link to="/">{item.productName}</Link>
-                                <Link to="/">{item.price}</Link>
+                                <Link to={`/${product.productId}`}>{product.productName}</Link>
+                                <Link to={`/${product.productId}`}>{product.price}</Link>
                             </div>
                         </li>
                     ))}
                 </ul>
             </div>
-            <FaChevronLeft className="left-arrow" onClick={moveLeft}/>
-            <FaChevronRight className="right-arrow" onClick={moveRight}/>
+            <FaChevronLeft className="product-slide-left-arrow" onClick={moveLeft}/>
+            <FaChevronRight className="product-slide-right-arrow" onClick={moveRight}/>
             <Link to="/" className="link-btn">Click Here to More View</Link>
         </section>
     );
