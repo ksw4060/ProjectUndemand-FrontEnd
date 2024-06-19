6121f79f1bcbe209715ae3041a4cc690042f631d
I Joo Han <juhanlee2218@gmail.com>
2024-06-19 17:20:12 +0900

    [Modify] 메인 페이지 스켈레톤 적용 #149

--- src/components/ProductSlide/ProductSlide.jsx
+++ src/components/ProductSlide/ProductSlide.jsx
@@ -8,7 +8,8 @@ import "swiper/css/effect-cards";
 import "./ProductSlide.css";
 import { Pagination, Navigation, EffectCards } from "swiper/modules";
 
-function ProductSlide({ products, sectionTitle }) {
+function ProductSlide({ loading, products, sectionTitle }) {
+  const skeletonSlides = Array.from({ length: 7 });
   const sectionType = sectionTitle.split(" ")[0];
   const sectionTypeUpperCase = sectionType.toUpperCase();
   const [swiper, setSwiper] = useState();
@@ -41,106 +42,159 @@ function ProductSlide({ products, sectionTitle }) {
         <h1>{sectionTitle}</h1>
       </div>
       <div className="product-slide-container">
-        {isWidth === false && (
-          <Swiper
-            slidesPerView={"auto"}
-            spaceBetween={20}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            onSwiper={(e) => {
-              setSwiper(e);
-            }}
-            modules={[Pagination, Navigation]}
-          >
-            {products.map((product) => (
-              <SwiperSlide
-                key={product.productId}
-                onClick={() => navigate(`/product/${product.productId}`)}
+        {loading ? (
+          <>
+            {isWidth === false && (
+              <Swiper
+                slidesPerView={"auto"}
+                spaceBetween={20}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                onSwiper={(e) => {
+                  setSwiper(e);
+                }}
+                modules={[Pagination, Navigation]}
               >
-                <div className="product-slide-card-container">
-                  <img
-                    src={`${process.env.REACT_APP_BACKEND_URL_FOR_IMG}${product.productThumbnails[0]}`}
-                    alt={product.productName}
-                    className={`product-card-img`}
-                  />
-                  <div className="product-small-info">
-                    <div className="pd-name-discount-recommend-wrapper">
-                      <Link to={`/product/${product.productId}`}>
-                        {product.productName}
-                      </Link>
-                      {product.isDiscount === true && (
+                {skeletonSlides.map((_, index) => (
+                  <SwiperSlide
+                    key={index}
+                    className="swiper-slide-skeleton"
+                  ></SwiperSlide>
+                ))}
+              </Swiper>
+            )}
+          </>
+        ) : (
+          <>
+            {isWidth === false && (
+              <Swiper
+                slidesPerView={"auto"}
+                spaceBetween={20}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                onSwiper={(e) => {
+                  setSwiper(e);
+                }}
+                modules={[Pagination, Navigation]}
+              >
+                {products.map((product) => (
+                  <SwiperSlide
+                    key={product.productId}
+                    onClick={() => navigate(`/product/${product.productId}`)}
+                  >
+                    <div className="product-slide-card-container">
+                      <img
+                        src={`${process.env.REACT_APP_BACKEND_URL_FOR_IMG}${product.productThumbnails[0]}`}
+                        alt={product.productName}
+                        className={`product-card-img`}
+                      />
+                      <div className="product-small-info">
+                        <div className="pd-name-discount-recommend-wrapper">
+                          <Link to={`/product/${product.productId}`}>
+                            {product.productName}
+                          </Link>
+                          {product.isDiscount === true && (
+                            <Link
+                              to={`/product/${product.productId}`}
+                            >{`${product.discountRate}% 할인 중`}</Link>
+                          )}
+                          {product.isRecommend === true && (
+                            <Link to={`/product/${product.productId}`}>
+                              추천상품!
+                            </Link>
+                          )}
+                        </div>
                         <Link
                           to={`/product/${product.productId}`}
-                        >{`${product.discountRate}% 할인 중`}</Link>
-                      )}
-                      {product.isRecommend === true && (
-                        <Link to={`/product/${product.productId}`}>
-                          추천상품!
+                          className="product-price"
+                        >
+                          {`${product.price} 원`}
                         </Link>
-                      )}
+                      </div>
                     </div>
-                    <Link
-                      to={`/product/${product.productId}`}
-                      className="product-price"
-                    >
-                      {`${product.price} 원`}
-                    </Link>
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            )}
+          </>
         )}
-        {isWidth === true && (
-          <Swiper
-            effect={"cards"}
-            grabCursor={true}
-            navigation={true}
-            onSwiper={(e) => {
-              setSwiper(e);
-            }}
-            modules={[EffectCards, Navigation]}
-          >
-            {products.map((product) => (
-              <SwiperSlide
-                key={product.productId}
-                onClick={() => navigate(`/product/${product.productId}`)}
+        {loading ? (
+          <>
+            {isWidth === true && (
+              <Swiper
+                effect={"cards"}
+                grabCursor={true}
+                navigation={true}
+                onSwiper={(e) => {
+                  setSwiper(e);
+                }}
+                modules={[EffectCards, Navigation]}
               >
-                <div className="product-slide-card-container">
-                  <img
-                    src={`${process.env.REACT_APP_BACKEND_URL_FOR_IMG}${product.productThumbnails[0]}`}
-                    alt={product.productName}
-                    className={`product-card-img`}
-                  />
-                  <div className="product-small-info">
-                    <div className="pd-name-discount-recommend-wrapper">
-                      <Link to={`/product/${product.productId}`}>
-                        {product.productName}
-                      </Link>
-                      {product.isDiscount === true && (
+                {skeletonSlides.map((_, index) => (
+                  <SwiperSlide
+                    key={index}
+                    className="swiper-slide-skeleton"
+                  ></SwiperSlide>
+                ))}
+              </Swiper>
+            )}
+          </>
+        ) : (
+          <>
+            {isWidth === true && (
+              <Swiper
+                effect={"cards"}
+                grabCursor={true}
+                navigation={true}
+                onSwiper={(e) => {
+                  setSwiper(e);
+                }}
+                modules={[EffectCards, Navigation]}
+              >
+                {products.map((product) => (
+                  <SwiperSlide
+                    key={product.productId}
+                    onClick={() => navigate(`/product/${product.productId}`)}
+                  >
+                    <div className="product-slide-card-container">
+                      <img
+                        src={`${process.env.REACT_APP_BACKEND_URL_FOR_IMG}${product.productThumbnails[0]}`}
+                        alt={product.productName}
+                        className={`product-card-img`}
+                      />
+                      <div className="product-small-info">
+                        <div className="pd-name-discount-recommend-wrapper">
+                          <Link to={`/product/${product.productId}`}>
+                            {product.productName}
+                          </Link>
+                          {product.isDiscount === true && (
+                            <Link
+                              to={`/product/${product.productId}`}
+                            >{`${product.discountRate}% 할인 중`}</Link>
+                          )}
+                          {product.isRecommend === true && (
+                            <Link to={`/product/${product.productId}`}>
+                              추천상품!
+                            </Link>
+                          )}
+                        </div>
                         <Link
                           to={`/product/${product.productId}`}
-                        >{`${product.discountRate}% 할인 중`}</Link>
-                      )}
-                      {product.isRecommend === true && (
-                        <Link to={`/product/${product.productId}`}>
-                          추천상품!
+                          className="product-price"
+                        >
+                          {`${product.price} 원`}
                         </Link>
-                      )}
+                      </div>
                     </div>
-                    <Link
-                      to={`/product/${product.productId}`}
-                      className="product-price"
-                    >
-                      {`${product.price} 원`}
-                    </Link>
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            )}
+          </>
         )}
         <div className="swiper-button-prev" onClick={handlePrev}></div>
         <div className="swiper-button-next" onClick={handleNext}></div>
